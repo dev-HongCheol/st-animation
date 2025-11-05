ba0bc01b4ae1155dcc55284e3fa8f39add28f024
Choi HongCheol <choisieun89@gmail.com>
2025-11-05 12:52:25 +0900

    feat: Hero 페이지 동영상 추가

---
+++ src/components/Button.tsx
@@ -0,0 +1,32 @@
+import type { HtmlHTMLAttributes, ReactNode } from "react";
+
+interface Props extends HtmlHTMLAttributes<HTMLButtonElement> {
+  leftIcon: ReactNode;
+  rightIcon?: ReactNode;
+  containerClass: string;
+}
+
+const Button = ({
+  containerClass,
+  title,
+  leftIcon,
+  rightIcon,
+  ...props
+}: Props) => {
+  return (
+    <button
+      {...props}
+      className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}
+    >
+      {leftIcon}
+
+      <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
+        <div>{title}</div>
+      </span>
+
+      {rightIcon && rightIcon}
+    </button>
+  );
+};
+
+export default Button;
